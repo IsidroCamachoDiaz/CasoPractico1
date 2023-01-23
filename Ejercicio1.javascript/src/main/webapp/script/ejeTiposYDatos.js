ffa7ab34b312cc7b449948ef58a84d907fd13634
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-01-23 09:53:48 +0100

    23-1-2023

---
+++ Ejercicio1.javascript/src/main/webapp/script/ejeTiposYDatos.js
@@ -0,0 +1,46 @@
+function cas1(){
+let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+}
+
+function cas2(){
+     let es = true;
+     document.write(typeof(es));
+     document.write(es);
+}
+
+function cas3(){
+    let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+}
+
+function cas4(){
+    let numeroGrande = 4567891324657987654;
+     let numeroGrandeCasteado = BigInt(987654321654987654);
+
+     document.write(typeof(numeroGrande));
+     document.write(numeroGrande);
+     document.write(typeof(numeroGrandeCasteado));
+    document.write(numeroGrandeCasteado);
+}
+
+function cas5(){
+    let texto = "Soy un string";     
+
+    document.write(typeof(texto));
+
+     document.write(texto);
+}
+
+function cas6(){
+    let variableVacia;
+     document.write(variableVacia);
+
+     document.write(typeof(variableVacia));
+}
